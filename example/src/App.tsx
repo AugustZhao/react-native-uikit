e6043db05842ce7e06aa375d75feb9b73730c57b
AugustZhao <august_zhao@sina.cn>
2022-06-25 17:58:35 +0800

    upgrade react version from 16.13.1 to 17.0.2 and react-native version from 0.63.4 to  0.68.0

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-uikit';
+import { multiply } from 'react-native-uikits';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
